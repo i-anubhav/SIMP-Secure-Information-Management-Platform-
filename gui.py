584e890db48768e5e15ac2d09b17cebf0bc5650c
Anubhav <55060684+i-anubhav@users.noreply.github.com>
2022-12-16 16:23:48 +0530

    Create gui.py

---
+++ gui.py
@@ -0,0 +1,28 @@
+import tkinter as tk
+from tkinter import filedialog, test, Button
+import os
+from simp
+
+root = tk.Tk()
+Button1 = Button(root, text="Open the sorting interface", padx=50, pady=50)
+Button1.pack(side=TOP)
+
+Button2= Button(root, text="open the Encryption interface", padx=50, pady=50)
+Button2.pack()
+
+
+root.mainloop()
+import tkinter as tk
+from tkinter import filedialog, test, Button
+import os
+from simp
+
+root = tk.Tk()
+Button1 = Button(root, text="Open the sorting interface", padx=50, pady=50)
+Button1.pack(side=TOP)
+
+Button2= Button(root, text="open the Encryption interface", padx=50, pady=50)
+Button2.pack()
+
+
+root.mainloop()
